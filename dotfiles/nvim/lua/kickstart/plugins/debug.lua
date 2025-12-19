e54b0d5fd9e29da3e25761908126c216ecf10fc2
Adrien Moreau <moreauadrien92@gmail.com>
2025-12-19 11:50:01 +0100

    update

--- dotfiles/nvim/lua/kickstart/plugins/debug.lua
+++ dotfiles/nvim/lua/kickstart/plugins/debug.lua
@@ -7,7 +7,9 @@
 -- kickstart.nvim and not kitchen-sink.nvim ;)
 
 return {
+  -- NOTE: Yes, you can install new plugins here!
   'mfussenegger/nvim-dap',
+  -- NOTE: And you can specify dependencies as well
   dependencies = {
     -- Creates a beautiful debugger UI
     'rcarriga/nvim-dap-ui',
@@ -16,7 +18,7 @@ return {
     'nvim-neotest/nvim-nio',
 
     -- Installs the debug adapters for you
-    'williamboman/mason.nvim',
+    'mason-org/mason.nvim',
     'jay-babu/mason-nvim-dap.nvim',
 
     -- Add your own debuggers here
