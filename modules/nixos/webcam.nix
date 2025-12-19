5ea8c1b28856930cc3b0c1467910056a2d90aa52
Adrien Moreau <moreauadrien92@gmail.com>
2025-12-19 13:30:17 +0100

    webcam update

--- modules/nixos/webcam.nix
+++ modules/nixos/webcam.nix
@@ -1,3 +1,15 @@
 {...}: {
-  hardware.ipu6.enable = true;
+  hardware.ipu6 = {
+    enable = true;
+    platform = "ipu6ep";
+  };
+  hardware.enableRedistributableFirmware = true;
+
+  users.users.adrien.extraGroups = [ "video" ];
+
+  environment.systemPackages = with pkgs; [
+    v4l-utils  # Pour lister et tester les caméras
+    ffmpeg     # Pour capturer de la vidéo
+    cheese     # Application simple pour tester la caméra (optionnel)
+  ];
 }
