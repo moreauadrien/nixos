a0ae40925c6751d1d42ae10561be2234e0b6d8ef
Adrien Moreau <moreauadrien92@gmail.com>
2025-12-30 19:16:21 +0100

    Generation 112

--- modules/nixos/bluetooth.nix
+++ modules/nixos/bluetooth.nix
@@ -4,4 +4,11 @@
   environment.systemPackages = with pkgs; [
     bluetui
   ];
+
+  systemd.user.services.mpris-proxy = {
+    description = "Mpris proxy";
+    after = ["network.target" "sound.target"];
+    wantedBy = ["default.target"];
+    serviceConfig.ExecStart = "${pkgs.bluez}/bin/mpris-proxy";
+  };
 }
