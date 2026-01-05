a92451f906db96675114d414af6be63fa503a055
Adrien Moreau <moreauadrien92@gmail.com>
2026-01-05 19:32:54 +0100

    Generation 131

--- hosts/tallyho/home.nix
+++ hosts/tallyho/home.nix
@@ -13,7 +13,7 @@
     ../../modules/home-manager/git.nix
     ../../modules/home-manager/scripts.nix
     ../../modules/home-manager/cursor.nix
-    ../../modules/home-manager/whatsapp.nix
+    #../../modules/home-manager/whatsapp.nix
   ];
 
   home.username = "adrien";
