bba78aaf09a6c779723223356f1d4ee9ae7a9930
Adrien Moreau <moreauadrien92@gmail.com>
2026-01-06 10:00:12 +0100

    Generation 139

--- hosts/tallyho/configuration.nix
+++ hosts/tallyho/configuration.nix
@@ -122,6 +122,12 @@ in {
     unstable.ollama
   ];
 
+  services.ollama = {
+    enable = true;
+    # Optional: preload models, see https://ollama.com/library
+    loadModels = ["deepseek-r1:1.5b"];
+  };
+
   services.udisks2.enable = true;
 
   programs.tmux.enable = true;
