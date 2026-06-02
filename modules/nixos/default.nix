b032193a4bbac9face6f76a88ee2b5074009e7e1
Adrien Moreau <adrienmoreau@ik.me>
2026-06-02 20:55:58 +0200

    Generation 303

--- modules/nixos/default.nix
+++ modules/nixos/default.nix
@@ -17,20 +17,18 @@
 
   programs.kdeconnect.enable = true;
 
-  networking.firewall = {
-    allowedTCPPortRanges = [
-      {
-        from = 1714;
-        to = 1764;
-      }
-    ];
-    allowedUDPPortRanges = [
-      {
-        from = 1714;
-        to = 1764;
-      }
-    ];
-  };
-
-  programs.wireshark.enable = true;
+  #networking.firewall = {
+  #  allowedTCPPortRanges = [
+  #    {
+  #      from = 1714;
+  #      to = 1764;
+  #    }
+  #  ];
+  #  allowedUDPPortRanges = [
+  #    {
+  #      from = 1714;
+  #      to = 1764;
+  #    }
+  #  ];
+  #};
 }
