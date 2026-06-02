9f72bd5749da71b1b3801a4711841f221eb6e118
Adrien Moreau <adrienmoreau@ik.me>
2026-06-02 15:08:23 +0200

    Generation 301

--- modules/nixos/default.nix
+++ modules/nixos/default.nix
@@ -30,20 +30,7 @@
         to = 1764;
       }
     ];
-    # Ou plus ciblé sur l'interface tailscale :
-    interfaces."tailscale0" = {
-      allowedTCPPortRanges = [
-        {
-          from = 1714;
-          to = 1764;
-        }
-      ];
-      allowedUDPPortRanges = [
-        {
-          from = 1714;
-          to = 1764;
-        }
-      ];
-    };
   };
+
+  programs.wireshark.enable = true;
 }
