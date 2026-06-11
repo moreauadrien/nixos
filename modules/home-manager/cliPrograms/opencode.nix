8d6364593c7fa4209374cda2a45dd60d92e925ff
Adrien Moreau <adrienmoreau@ik.me>
2026-06-11 15:25:48 +0200

    Declare opencode settings via home manager

--- modules/home-manager/cliPrograms/opencode.nix
+++ modules/home-manager/cliPrograms/opencode.nix
@@ -1,5 +1,12 @@
-{...}: {
+{pkgs, ...}: {
   programs.opencode = {
     enable = true;
+    extraPackages = with pkgs; [
+      wl-clipboard
+    ];
+
+    settings = {
+      plugin = ["@mohak34/opencode-notifier@latest"];
+    };
   };
 }
