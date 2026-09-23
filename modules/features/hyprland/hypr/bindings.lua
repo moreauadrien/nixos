58418e2304044848f0ab294cd843b44d19777f68
Adrien Moreau <adrienmoreau@ik.me>
2026-09-23 16:36:20 +0200

    feat: add push to talk for hyprland

--- modules/features/hyprland/hypr/bindings.lua
+++ modules/features/hyprland/hypr/bindings.lua
@@ -29,3 +29,7 @@ hl.bind("XF86AudioNext",  hl.dsp.exec_cmd("playerctl next"),       { locked = tr
 hl.bind("XF86AudioPause", hl.dsp.exec_cmd("playerctl play-pause"), { locked = true })
 hl.bind("XF86AudioPlay",  hl.dsp.exec_cmd("playerctl play-pause"), { locked = true })
 hl.bind("XF86AudioPrev",  hl.dsp.exec_cmd("playerctl previous"),   { locked = true })
+
+
+hl.bind("twosuperior", hl.dsp.exec_cmd("voxtype record start"))
+hl.bind("twosuperior", hl.dsp.exec_cmd("voxtype record stop"), { release = true })
