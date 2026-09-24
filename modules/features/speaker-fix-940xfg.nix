037faf57e9c69c3bcf5bda976cd5c22131b5820c
Adrien Moreau <adrienmoreau@ik.me>
2026-09-24 19:43:09 +0200

    fix: galaxy pro book 3 speaker fix

--- modules/features/speaker-fix-940xfg.nix
+++ modules/features/speaker-fix-940xfg.nix
@@ -1,7 +1,9 @@
 { inputs, ... }:
 {
   flake.nixosModules.speaker-fix-940xfg = { ... }: {
-    imports = [ "${inputs.samsung-fixes}/nixos/speaker-fix-940xfg.nix" ];
+    imports = [
+      "${inputs.samsung-fixes}/nixos/speaker-fix-940xfg.nix"
+    ];
     hardware.samsungGalaxyBook.speakerFix940xfg.enable = true;
   };
 }
